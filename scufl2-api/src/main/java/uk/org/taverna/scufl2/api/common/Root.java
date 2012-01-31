a8e57829e3cdbfbe38a338160201193b9cc1a935
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2012-01-31 15:27:22 +0000

    removed sourcecode from gh-pages

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/common/Root.java
+++
@@ -1,9 +0,0 @@
-package uk.org.taverna.scufl2.api.common;
-
-import java.net.URI;
-
-public interface Root extends WorkflowBean {
-	public URI getGlobalBaseURI();
-
-	public void setGlobalBaseURI(URI globalBaseURI);
-}
--- scufl2-rdfxml/src/test/resources/uk/org/taverna/scufl2/rdfxml/update-bundle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-rm example.scufl2
-cd example
-zip -0 -X ../example.scufl2 mimetype
-zip -r ../example.scufl2 . -x mimetype 
